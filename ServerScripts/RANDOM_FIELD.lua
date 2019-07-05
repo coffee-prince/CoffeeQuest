aa4e265107498bff44a0f09fac1439f7bcf94737
sksky1990 <52263743+sksky1990@users.noreply.github.com>
2019-07-05 19:47:53 +0900

    2단계

--- ServerScripts/RANDOM_FIELD.lua
+++ ServerScripts/RANDOM_FIELD.lua
@@ -8,14 +8,38 @@ Server.GetTopic("RANDOM_FIELD").Add(function()
 
 	local level = math.floor(sum / 10)
 
-	local ex = math.random(5, math.min(50, math.max(sum * 4, 10)))
+	if sum <= 10 then
+
+		level = 0
+
+	elseif sum <= 25 then
+		level = 1
+
+	elseif sum <= 45 then
+		level = 2
+
+	elseif sum <= 65 then
+		level = 3
+
+	elseif sum <= 85 then
+		level = 4
+
+	else
+		level = 5
+	end
+
+
+	local ex = math.random(5, 20)
+
+
+
 	--local ex = math.random(5,200)
 
 	field.SetFieldVar(0, ex)
 
 	for i = 1, ex do
 		local monster_id = math.random(7, 12)
-		monster_id = monster_id  + 5 * math.random(0, math.min(level, 6))
+		monster_id = monster_id  + 5 * math.random(0, math.min(level, 5))
 
 		--print(monster_id)
 
