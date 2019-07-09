cf0bff81f54f6f2fb13baf44663d10f3931e59cc
GodHongIl <adunstudio@daum.net>
2019-07-09 20:46:32 +0900

    정령 이름 수정

--- ServerScripts/KILL_BOSS.lua
+++ ServerScripts/KILL_BOSS.lua
@@ -5,7 +5,9 @@ Server.GetTopic("KILL_BOSS_REQUEST").Add(function(bossID)
 	unit.SetVar(id, 1)
 
 	if unit.GetVar(151) + unit.GetVar(152) + unit.GetVar(153) + unit.GetVar(154) + unit.GetVar(155) + unit.GetVar(156) == 6 then
-		unit.name = "<color=#DF3A01>모은자</color> " .. unit.name
+
+		unit.name = "<color=#DF3A01>커피 정복자</color> "
+
 	end
 
 	unit.FireEvent("KILL_BOSS_RESPONSE", unit.GetVar(151), unit.GetVar(152), unit.GetVar(153), unit.GetVar(154), unit.GetVar(155), unit.GetVar(156)  )
