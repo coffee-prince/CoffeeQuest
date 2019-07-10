a1b7b9aa125ff01b3d469377fef0657259e6a058
GodHongIl <adunstudio@daum.net>
2019-07-10 09:52:37 +0900

    골드수정

--- ServerScripts/RANDOM_DEFENSE.lua
+++ ServerScripts/RANDOM_DEFENSE.lua
@@ -33,23 +33,4 @@ Server.GetTopic("RANDOM_DEFENSE").Add(function()
 
 		field.SpawnEnemy(math.random(60, 62), spawn_x, spawn_y)
 	end
-
-
-
-	--local ex = math.random(5,200)
-
-	for i = 1, ex do
-		local monster_id = math.random(7, 12)
-		monster_id = monster_id  + 5 * math.random(0, math.min(level, 6))
-
-
-
-		--print(monster_id)
-
-		local spawn_x = math.random(32 * 4, 32 * 28)
-		local spawn_y = math.random(-32 * 6, -32 * 18)
-
-
-	end
-
 end)
